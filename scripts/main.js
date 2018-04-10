0d1805e133286926d37db6f016f39b3b026030b4
JessicaSteeman <jessicast@live.nl>
2018-04-10 18:22:08 +0200

    Changed Content

---
+++ scripts/main.js
@@ -0,0 +1,387 @@
+
+// on page load show map
+document.onload = function() {
+	initMap();
+};
+
+// init map
+var myMap;
+
+var landingSpots = [
+	{
+		name: 'Kennedy Space Center',
+		location: {lat: 28.5728722,lng: -80.6511695},
+        ranking: 2
+	}, {
+		name: 'Green Swamp',
+		location: {lat: 28.4361811, lng: -81.96394183},
+        ranking: 5
+	}, {
+		name: 'Everglades',
+		location: {lat: 25.97087439,lng: -81.08866618},
+        ranking: 0
+	}, {
+		name: 'Lake Okeechobee',
+		location: {lat: 26.94475043, lng: -80.9055606},
+        ranking: 4
+	}, {
+		name: 'Ocala',
+		location: {lat: 29.06706271, lng: -81.74421526},
+        ranking: 3
+	}
+];
+
+
+
+var icons = [
+	'images/rocket.png', 
+	'images/rocket.png',
+	'images/rocket.png',
+	'images/cross.png',
+	'images/cross.png',
+	'images/cross.png'
+];
+
+function initMap() {
+    
+	// set options for map 
+	var mapOptions = {
+		center: {
+			lat: 28.20891491, 
+			lng: -81.674859
+		},
+		zoom: 7,
+        styles: [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#212121"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#212121"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#bdbdbd"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#181818"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1b1b1b"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#2c2c2c"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#8a8a8a"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#373737"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#3c3c3c"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#4e4e4e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#3d3d3d"
+                    }
+                ]
+            }
+        ]
+        
+	};
+
+	// create map and add to page
+	myMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	for(var i=0; i<landingSpots.length; i++){
+		addSpot(landingSpots[i]);
+	}
+    
+    myMap.addListener('center_changed', function() {
+        // 3 seconds after the center of the map has changed, pan back to the
+        // marker.
+        window.setTimeout(function() {
+            myMap.panTo(landingSpots.getPosition());
+        }, 3000);
+    });
+    
+}
+
+function addSpot(spot){
+	new google.maps.Marker({
+		position: spot.location,
+		map: myMap,
+		title: spot.name,
+		icon: icons[spot.ranking]
+	});
+    
+	console.log(spot.name + ' added to map');
+       
+}
+
+
+
+/**
+ * Fetch API data
+ */
+function getAPIdata() {
+	
+	var url = "http://api.openweathermap.org/data/2.5/forecast";
+	var apiKey ="7aecb9aea729ce90c2e7f44b4ee54fa6";
+	var city = "Florida";
+
+	// construct request
+	var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
+	
+	// get weather forecast
+	fetch(request)
+
+	// parse to JSON format
+	.then(function(response) {
+		return response.json();
+	})
+	
+	// render weather per day
+	.then(function(response) {
+
+		// render weatherCondition
+		onAPISucces(response);
+	})
+	
+	// catch error
+	.catch(function (error) {
+		// onAPIError();
+		console.error('Request failed', error);
+	});
+}
+
+/**
+ * Render weather listing
+ */
+function onAPISucces(response) {
+
+	var weatherList = response.list;
+	var weatherBox = document.getElementById('weather');
+
+	for(var i=0; i< weatherList.length; i++){
+		//console.log(weatherList[i].main.temp - 273.15);
+
+		var dateTime = new Date(weatherList[i].dt_txt);
+		var date = formDate(dateTime);
+		var time = formTime(dateTime);
+		var temp = Math.floor(weatherList[i].main.temp - 273.15);
+
+		forecastMessage =  '<div class="forecastMoment">';
+		forecastMessage +=   '<div class="date"> '+date+' </div>';
+		forecastMessage +=	 '<div class="time"> '+ time +' </div>';
+		forecastMessage +=	 '<div class="temp"> '+temp+'&#176;C </div>';
+		forecastMessage += '</div>';
+
+		weatherBox.innerHTML += forecastMessage;
+	}
+}
+
+/**
+ * Error
+ */
+function updateUIError() {
+	var weatherBox = document.getElementById('weather');
+	weatherBox.className = 'hidden'; 
+}
+
+/**
+ * Format date
+ */
+function formDate(date) {
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	return day +'/'+ month;
+}
+
+/**
+ * Format time
+ */
+function formTime(date) {
+	var hours = date.getHours();
+	if(hours<10){
+		hours = '0'+hours;
+	}
+	var minutes = date.getMinutes();
+	if(minutes < 10){
+		minutes = '0'+ minutes;
+	}
+	return hours +':'+ minutes;
+}
+
+// init data stream
+getAPIdata();
+
+
